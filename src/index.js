ef16eee0cec275a4d5675d0a48a9231a1ba1c63d
Matt Hakes <vficto@gmail.com>
2021-11-30 15:22:51 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 /* jshint esversion: 10 */
 import "./styles.css";
 import amp from "./amp.js";
-console.clear();
+//
 const cl = (...args) => console.log(...args);
-
+//
 const myMath = function myMath() {};
 amp(myMath);
 const even = (v) => Number.isSafeInteger(v) && v % 2 === 0;
 myMath.quick("even", even);
 const arr = [1, 2, "x", [], 3, 4, {}, 5, "y", 6];
 const r = myMath.quickEven(arr);
-cl(r);
-cl(Object.keys(r).length);
